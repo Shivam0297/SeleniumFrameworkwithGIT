ef37efae9aa81644122d38c49c5aecff6eb0ea86
Shivam Pandey <49114010+Shivam0297@users.noreply.github.com>
2025-04-03 09:03:12 +0530

    test

--- src/main/java/pageObject/LoginPageElements.java
+++ src/main/java/pageObject/LoginPageElements.java
@@ -2,7 +2,7 @@ package pageObject;
 
 public interface LoginPageElements {
 
-	 String loginText= "/html/body/div[1]/header[1]/div[1]/div[3]/a[2]";
+	 //String loginText= "/html/body/div[1]/header[1]/div[1]/div[3]/a[2]";
 	 String emailAddress = "//input[@placeholder='Email']";
 	 String passwordField="//input[@placeholder='Password']";
 	
